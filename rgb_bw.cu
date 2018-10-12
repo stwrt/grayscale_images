dc604d6c1220a5672147a14c9fe545be713cd1c3
Brandon Stewart <stewartb2@gmail.com>
2018-10-12 17:36:36 -0400

    Apply reimaging to both green and blue too

--- rgb_bw.cu
+++ rgb_bw.cu
@@ -17,6 +17,8 @@ __global__ void color_to_grey(uchar3 *input_image, uchar3 *output_image, int wid
     {
         int pos = row * width + col;
         output_image[pos].x = static_cast<unsigned char>(input_image[pos].x * 0.2126f + input_image[pos].y * 0.7125f + input_image[pos].z * 0.0722f);
+        output_image[pos].y = static_cast<unsigned char>(input_image[pos].x * 0.2126f + input_image[pos].y * 0.7125f + input_image[pos].z * 0.0722f);
+        output_image[pos].z = static_cast<unsigned char>(input_image[pos].x * 0.2126f + input_image[pos].y * 0.7125f + input_image[pos].z * 0.0722f);
     }
 }
 
